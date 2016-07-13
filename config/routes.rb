8b75fc3c83a896da35836e496566971e6e80a235
Diego Collares <diego.collares@gmail.com>
2016-07-12 23:18:52 -0300

    change route to users

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
+  
   # For details on the DSL available within this file, 
   #see http://guides.rubyonrails.org/routing.html
 
   namespace :ad do
+  	devise_for :users, controllers: {
+      sessions: 'ad/sessions'
+    }
   	root 'pages#index'
   end
 end
