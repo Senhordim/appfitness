85d40ca7637a7db997607320bee7452459f700b3
Diego Collares <diego.collares@gmail.com>
2016-07-28 18:44:13 -0300

    annotate model

--- app/models/academy.rb
+++ app/models/academy.rb
@@ -1,3 +1,25 @@
+# == Schema Information
+#
+# Table name: academies
+#
+#  id          :integer          not null, primary key
+#  name        :string
+#  description :text
+#  cnpj        :string
+#  phone       :string
+#  user_id     :integer
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
+# Indexes
+#
+#  index_academies_on_user_id  (user_id)
+#
+# Foreign Keys
+#
+#  fk_rails_b719417cd0  (user_id => users.id)
+#
+
 class Academy < ApplicationRecord
   belongs_to :user
 end
