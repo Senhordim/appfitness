313f080d33f0e72ebf3244b49be1a6573228c935
Diego Collares <diego.collares@gmail.com>
2016-07-29 00:43:50 -0300

    has_many acaemy in model

--- app/models/user.rb
+++ app/models/user.rb
@@ -31,7 +31,9 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :confirmable, :recoverable, :rememberable, :trackable, :validatable
+          :recoverable, :rememberable, :trackable, :validatable
 
   has_many :academies
+
+
 end
