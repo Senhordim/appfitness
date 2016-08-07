0ff90e9fe21174b65b25e7ac1840d2f0587ba310
Diego Collares <diego.collares@gmail.com>
2016-08-07 12:11:32 -0300

    hepler para flash notice

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,16 @@
 module ApplicationHelper
+	def bootstrap_class_for flash_type
+    case flash_type
+      when :success
+        "ls-alert-success"
+      when :error
+        "ls-alert-danger"
+      when :alert
+        "alert-block"
+      when :notice
+        "ls-alert-info"
+      else
+        flash_type.to_s
+    end
+  end
 end
